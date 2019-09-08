37c3c3ad54b6dca296075bc796c32bd59cf2817b
dtttt <1294889047@qq.com>
2019-09-08 21:20:47 +0800

    9.3

--- eleme/src/router/index.js
+++ eleme/src/router/index.js
@@ -63,9 +63,9 @@ const router = new Router({
 
 
 
-router.beforeEach((to,from,next)=>{
-   document.title = to.matched[0].meta.title
-    next()
-})
+// router.beforeEach((to,from,next)=>{
+//    document.title = to.matched[0].meta.title
+//     next()
+// })
 
 export default router
